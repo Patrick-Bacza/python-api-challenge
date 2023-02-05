74bb1f641bf86e1d9d35e92e51fc669edaff0f4d
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-02-05 17:51:37 -0500

    updates

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "dff45efb4cc070eab252244deeb14d38"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c7926f009c5e47c48af153aa993d4277"
